4783cff0c7c6ae635f9d32bfb5e0a899777e301a
PMagombedze <percymagoras@outlook.com>
2024-03-19 19:05:44 +0200

    update 2 hypermedia

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
+
 """
-Hypermedia Pagination
+hypermedia pagination
 """
+
 
 import csv
 from typing import Dict, List, Tuple, Union
+
 
 class Server:
     """
@@ -31,16 +34,16 @@ class Server:
     @staticmethod
     def index_range(page: int, page_size: int) -> Tuple[int, int]:
         """
-        Calculate index range
+        calculate index range
         """
         nextPageStartIndex = page * page_size
         return nextPageStartIndex - page_size, nextPageStartIndex
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
-        Get page
+        get page
         """
-        assert isinstance(page, int) and isinstance(page_size, int)
+        assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
         startIndex, endIndex = self.index_range(page, page_size)
         return self.dataset()[startIndex:endIndex]
@@ -48,19 +51,17 @@ class Server:
     def get_hyper(self, page: int,
                   page_size: int) -> Dict[str, Union[int, List[List], None]]:
         """
-        Get hyper
+        get hyper
         """
         data = self.get_page(page, page_size)
         totalRows = len(self.dataset())
         prev_page = page - 1 if page > 1 else None
-        next_page = page + 1 if self.index_range(page, page_size)[1] < totalRows else None
-        total_pages = totalRows // page_size + (1 if totalRows % page_size != 0 else 0)
-
-        return {
-            'page_size': len(data),
-            'page': page,
-            'data': data,
-            'next_page': next_page,
-            'prev_page': prev_page,
-            'total_pages': total_pages
-        }
+        next_page = page + 1
+        if self.index_range(page, page_size)[1] >= totalRows:
+            next_page = None
+        total_pages = totalRows / page_size
+        if total_pages % 1 != 0:
+            total_pages += 1
+        return {'page_size': len(data), 'page': page,
+                'data': data, 'next_page': next_page,
+                'prev_page': prev_page, 'total_pages': int(total_pages)}
